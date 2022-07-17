f970ce972443b82510493c322a82b29188d54e3e
JoonaManninen <joona.manninen7@gmail.com>
2022-07-17 15:51:05 +0300

    Third application done.

---
+++ app/src/main/java/com/example/exercise_3_joona_manninen/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.exercise_3_joona_manninen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView myListView;
+    String[] items;
+    String[] prizes;
+    String[] descriptions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        myListView = (ListView) findViewById(R.id.myListView);
+        items = res.getStringArray(R.array.items);
+        prizes = res.getStringArray(R.array.prizes);
+        descriptions = res.getStringArray(R.array.descriptions);
+
+        ItemAdapter itemAdapter = new ItemAdapter(this, items, prizes, descriptions);
+        myListView.setAdapter(itemAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent showDetailActivity = new Intent(getApplicationContext(), DetailActivity.class);
+                showDetailActivity.putExtra("com.example.ITEM_INDEX", i);
+                startActivity(showDetailActivity);
+            }
+        });
+    }
+}
